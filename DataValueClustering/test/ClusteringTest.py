b88a6abccb58d0484fc0bc61e497a9b2d0dede51
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-01-05 10:18:38 +0100

    Add test for Clustering.fancy_cluster_representation()

---
+++ DataValueClustering/test/ClusteringTest.py
@@ -0,0 +1,10 @@
+from main.Clustering import fancy_cluster_representation
+
+
+def test_fancy_cluster_representation():
+    values = ["a", "b", "c", "d", "e"]
+    clusters = [0, 0, 1, 2, 0]
+    assert fancy_cluster_representation(values, clusters) == [['a', 'b', 'e'], ['c'], ['d']]
+
+
+test_fancy_cluster_representation()
