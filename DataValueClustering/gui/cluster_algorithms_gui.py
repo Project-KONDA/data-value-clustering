5e45092b104750831e9e4cdfe847f7b3a1cf805b
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-01-05 14:26:36 +0100

    Add return statements to methods in cluster_algorithms_gui.py

--- DataValueClustering/gui/cluster_algorithms_gui.py
+++ DataValueClustering/gui/cluster_algorithms_gui.py
@@ -1,5 +1,9 @@
 from clustering.hierarchical import generate_linkage_matrix, hierarchical_lm
 from clustering.kmedoids import kmedoids
+from clustering.dbscan import *
+from clustering.optics import *
+from clustering.affinity_propagation import *
+from clustering.spectral import *
 
 from gui.dendrogram import show_dendrogram
 
@@ -13,7 +17,7 @@ def cluster_hierarchical(distance_function, values):
     linkage_matrix = generate_linkage_matrix(distance_function, values, method)
     show_dendrogram(linkage_matrix, values)
 
-    # TODO: ask for additional arguments: n_cluster, distance_threshold
+    # TODO: ask for additional arguments: n_cluster, distance_threshold etc.
     n_clusters = 2
     distance_threshold = 2
     criterion = 'distance'
@@ -37,23 +41,55 @@ def cluster_kmedoids(distance_function, values):
 
 def cluster_dbscan(distance_function, values):
     # TODO: ask user for arguments
-    # return dbscan(...)
-    pass
+    eps = 0.5
+    min_samples = 5
+    algorithm = 'auto'
+    leaf_size = 30
+    p = None
+    n_jobs = None
+
+    return dbscan(distance_function, values, eps, min_samples,algorithm, leaf_size, p, n_jobs)
 
 
 def cluster_optics(distance_function, values):
     # TODO: ask user for arguments
-    # return optics(...)
-    pass
+    min_samples = 5
+    max_eps = np.inf
+    cluster_method = 'xi'
+    eps = None
+    xi = 0.05
+    predecessor_correction = True
+    min_cluster_size = None
+    algorithm = 'auto'
+    leaf_size = 30
+    n_jobs = None
+
+    return optics(distance_function, values, min_samples, max_eps, cluster_method, eps, xi, predecessor_correction, min_cluster_size, algorithm, leaf_size, n_jobs)
 
 
 def cluster_affinity(distance_function, values):
     # TODO: ask user for arguments
-    # return affinity(...)
-    pass
+    damping = 0.5
+    max_iter = 200
+    convergence_iter = 15
+    copy = True
+    preference = None
+    verbose = False
+    random_state = None
+
+    return affinity(distance_function, values, damping, max_iter, convergence_iter, copy, preference, verbose, random_state)
 
 
 def cluster_spectral(distance_function, values):
     # TODO: ask user for arguments
-    # return spectral.py(...)
-    pass
+    n_clusters = 8
+    eigen_solver = None
+    n_components = 8
+    random_state = None
+    n_init = 10
+    gamma = 1.0
+    eigen_tol = 0.0
+    assign_labels = 'kmeans'
+    verbose = False
+
+    return spectral(distance_function, values, n_clusters, eigen_solver, n_components, random_state, n_init, gamma, eigen_tol, assign_labels, verbose)
