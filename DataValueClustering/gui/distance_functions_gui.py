8a99a3f8475cec6c906cdfdd3f461f8af569caf8
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-01-06 09:38:01 +0100

    Add distance_functions_gui.py

---
+++ DataValueClustering/gui/distance_functions_gui.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from distance.dice_coefficient import dice_coefficient_distance
+from distance.levenshtein_distance import levenshtein_distance
+from distance.longest_common_subsequence_distance import longest_common_subsequence_distance
+from distance.weighted_levenshtein_distance import weighted_levenshtein_distance
+
+
+def distance_dice():
+    return dice_coefficient_distance
+
+
+def distance_levenshtein():
+    return levenshtein_distance
+
+
+def distance_longest_common_subsequence():
+    return longest_common_subsequence_distance
+
+
+def distance_weighted_levenshtein():
+    # TODO: let user specify cost map
+    cost_map = None
+
+    return lambda s1,s2: weighted_levenshtein_distance(cost_map, s1, s2)
+
+
+distance_functions = np.array([
+    ["Dice",
+         distance_dice],
+    ["Levenshtein",
+         distance_levenshtein],
+    ["Longest Common Subsequence",
+         distance_longest_common_subsequence],
+    ["Weighted Levenshtein",
+         distance_weighted_levenshtein]
+])
+
