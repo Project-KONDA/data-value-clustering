925a5cde6da765eb8ded1b69e4392c469642aff0
kesperar <arno.kesper@uni-marburg.de>
2021-01-13 16:31:28 +0100

    Finalize image creation. Add image resize mechanism.

--- DataValueClustering/gui_distances/matrix_blob_input.py
+++ DataValueClustering/gui_distances/matrix_blob_input.py
@@ -3,24 +3,26 @@ from tkinter import Tk, Button, Canvas
 
 from PIL import Image, ImageTk
 
-
 import numpy as np
 
 
 class Blob:
-    def __init__(self, frame, canvas, label, x, y, size):
-        self.frame = frame
-        self.canvas = canvas
+    def __init__(self, blobinput, label, x, y, size):
+        self.blobinput = blobinput
         self.label = label
+        self.path = "..\\blob_images\\" + self.label + ".png"
         self.x = x
         self.y = y
         self.size = size
-        # self.oval = self.create_oval("white")
+        self.min_size = 50
+        self.max_size = 550
+        self.photoimage = None
         self.image = self.create_image()
 
     # size of a blob
     def get_size(self):
-        pass
+
+        return self.size - self.min_size
 
     # calculate distance to point or blob
     def get_distance(self, x=0, y=0, blob=None):
@@ -33,20 +35,23 @@ class Blob:
         self.x += dx
         self.y += dy
         # self.canvas.move(self.oval, dx, dy)
-        self.canvas.move(self.image, dx, dy)
+        self.blobinput.canvas.move(self.image, dx, dy)
 
     def set_position(self, x=0, y=0):
         self.x = x
         self.y = y
 
-    def scale_up(self):
-        pass
-
-    def scale_down(self):
-        pass
+    def scale(self, up=True):
+        if up:
+            self.size = min(self.max_size, self.size + 5)
+            # self.size = int(round(self.size * 1.251))
+        else:
+            self.size = max(self.min_size, self.size - 5)
+            # self.size = int(round(self.size / 1.251))
+        self.update_image()
 
     def create_oval(self, color):
-        return self.canvas.create_oval(
+        return self.blobinput.canvas.create_oval(
             self.x - 25,
             self.y - 25,
             self.x + 25,
@@ -57,67 +62,83 @@ class Blob:
         )
 
     def create_image(self):
-        # image = PhotoImage(file='../blob_images/' + self.label + '.png')  # TODO
-        # im = PIL.Image.open('../blob_images/' + self.label + '.png')
-        # photo = PIL.ImageTk.PhotoImage(im)
-        p = "../blob_images/" + self.label + ".png"
-        print(p)
-        load = Image.open(p)
-        print(load)
-        photo = ImageTk.PhotoImage(load)
-        print(photo)
+        img = Image.open(self.path)
+        img = img.resize((int(self.size * 1.2), self.size), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        print ('fail')
+        self.photoimage = img
+        return self.blobinput.canvas.create_image(self.x, self.y, image=img, anchor='center', tags=("token",))
 
-        return self.canvas.create_image(self.x, self.y, image=photo, anchor='center',tags=("token",))
+    def update_image(self):
+        if self.image is not None:
+            img = Image.open(self.path)
+            img = img.resize((int(self.size * 1.2), int(self.size)), Image.ANTIALIAS)
+            img = ImageTk.PhotoImage(img)
+            self.photoimage = img
+            # self.image =
+            self.blobinput.canvas.itemconfig(self.image, image=img)
 
 
 class BlobInput:
 
     def __init__(self, chars_labels):
 
+        self.chars_labels = chars_labels
         self.labels = chars_labels[:, 1]
         self.chars = chars_labels[:, 0]
 
+        """ROOT"""
         self.root = Tk()
         self.root.title('Distance Specification')
-        self.root.configure(background='white')
 
-        self.w = 900
-        self.h = 700
+        """Frame"""
+        self.w = 1000
+        self.h = 800
         self.root.geometry(str(self.w) + "x" + str(self.h))
+        self.root.config(bg='white')
         self.x = int(self.w / 2)
         self.y = int(self.h / 2)
+        self.diagonal = int(sqrt(self.w * self.w + self.h * self.h))
 
-        # create a canvas
-        self.canvas = Canvas(width=400, height=400, background="bisque")
+        """Images"""
+        print(self.diagonal)
+        self.image_sizes = int(min(self.h, self.w) / 8)
+        self.max_distance = 20
+        self.distance_factor = self.max_distance / self.diagonal
+        self.size_factor = self.max_distance / self.diagonal
+        self.distance_threashold = self.diagonal/20
+        self.coordinates = self.create_coordinates()  # array: [label, x, y, size]
+
+        """Canvas"""
+        self.canvas = Canvas(width=400, height=400, background="alice blue")
         self.canvas.pack(fill="both", expand=True)
 
-        # this data is used to keep track of an
-        # item being dragged
+        """Dragging"""
+        # this data is used to keep track of an item being dragged
         self._drag_data = {"x": 0, "y": 0, "item": None, "item_last": None}
-
-        # add bindings for clicking, dragging and releasing over
-        # any object with the "token" tag
+        # add bindings for clicking, dragging and releasing over any object with the "token" tag
         self.canvas.tag_bind("token", "<ButtonPress-1>", self.drag_start)
         self.canvas.tag_bind("token", "<ButtonRelease-1>", self.drag_stop)
         self.canvas.tag_bind("token", "<B1-Motion>", self.drag)
+        self.canvas.bind_all("<MouseWheel>", self.scale_a_blob)
 
-        # array: [label, x, y, size]
-        self.coordinates = self.create_coordinates()
-        #print(self.coordinates)
-
-        # build blobs
+        """Build Blobs"""
         self.blobs = np.empty(len(chars_labels), dtype=Blob)
         for i, c in enumerate(self.coordinates):
-            self.blobs[i] = Blob(self.root, self.canvas, c[0], c[1], c[2], c[3])
+            self.blobs[i] = Blob(self, c[0], c[1], c[2], c[3])
 
-        Button(self.root, text='OK', command=self.close, width=int(self.w/7.2), background='white'
+        """OK Button"""
+        Button(self.root, text='OK', command=self.close, width=int(self.w / 7.2), background='lime green'
+               ).place(anchor='center', x=self.x, y=self.h - 50)
+
+        Button(self.root, text='Restart', command=self.restart, width=int(self.w / 7.2), background='brown2'
                ).place(anchor='center', x=self.x, y=self.h - 20)
 
         self.root.mainloop()
 
     # create n coordinates of equal distance to middlepoint x|y clockwise
     def create_coordinates(self):
-        distance_to_center = min(self.w, self.h)/4
+        distance_to_center = min(self.w, self.h) / 4
         # array: [label, x, y, size]
         array = np.empty((len(self.labels), 4), dtype=object)
         degree_delta = 360 / len(self.labels)
@@ -129,14 +150,15 @@ class BlobInput:
             array[i, 0] = l
             array[i, 1] = self.x + a
             array[i, 2] = self.y + g
-            array[i, 3] = 10
+            array[i, 3] = self.image_sizes
 
         return array
 
     def find_nearest_blob(self, x, y):
-        min_blob, min_distance = (self.blobs[0], self.blobs[0].get_distance(x, y))
+        min_blob, min_distance = (None, self.distance_threashold)
+        #min_blob, min_distance = (self.blobs[0], self.blobs[0].get_distance(x, y))
         for i, blob in enumerate(self.blobs):
-            d = blob.get_distance(x,y)
+            d = blob.get_distance(x, y) - blob.size
             if d < min_distance:
                 min_blob, min_distance = blob, d
         return min_blob
@@ -168,13 +190,23 @@ class BlobInput:
         self._drag_data["x"] = event.x
         self._drag_data["y"] = event.y
 
+    def scale_a_blob(self, event):
+        """Handles scaling a Blob"""
+        up = event.delta > 0
+        nearest = self.find_nearest_blob(event.x, event.y)
+        if nearest is not None:
+            nearest.scale(up)
+            print(nearest.label + " " + str(nearest.size))
+        else:
+            print("None")
+
     # calculate and return distance map
     def get(self):
         # TODO: () -> ?
         distance_map = {(()): 1.}
 
         for i, l in enumerate(self.labels):
-            distance_map[(i)] = "[^" + self.chars[i] + "$]"
+            distance_map[i] = "[^" + self.chars[i] + "$]"
 
         # TODO: costs for insertion (:,0) and deletion (0,:)
 
@@ -182,18 +214,24 @@ class BlobInput:
             for j, label_j in enumerate(self.labels):
                 if i == j:
                     # (i,j), i==j -> blobsize(i)
-                    distance_map[(i+1, i+1)] = self.blobs[i].get_size()
+                    distance_map[(i + 1, i + 1)] = round((self.blobs[i].get_size() * self.size_factor), 2)
                 else:
                     # (i,j), i!=j -> distance ( blob(i), blob(j) )
-                    distance_map[(i+1, j+1)] = self.blobs[i].get_distance(blob=self.blobs[j])
+                    distance_map[(i + 1, j + 1)] = round(
+                        (self.blobs[i].get_distance(blob=self.blobs[j]) * self.distance_factor), 2)
 
         return distance_map
 
     def close(self):
         self.root.destroy()
 
+    def restart(self):
+        chars_labels = self.chars_labels
+        self.root.destroy()
+        self.__init__(chars_labels)
+
 
 if __name__ == '__main__':
     # , ["2", "Blab"], ["3", "Blob"], ["4", "Blub"], ["5", "Blöb"], ["6", "Bläb"], ["7", "Blüb"]
-    names = np.array([["1", "Blib"]], dtype=object)
+    names = np.array([["1", "Digits"], ["1", "Letters"], ["1", "Integers"]], dtype=object)
     print(str(BlobInput(names).get()))
