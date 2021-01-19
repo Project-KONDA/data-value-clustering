535bbe61dd1bd6ebe51d5f7a2ea147c66cf66b55
kesperar <arno.kesper@uni-marburg.de>
2021-01-19 12:44:58 +0100

    Add QuestionnaireInput. Modify question_array.

--- DataValueClustering/compression/compression.py
+++ DataValueClustering/compression/compression.py
@@ -86,92 +86,94 @@ def word_sequence_compression_function(values, unify_values=True):
 
 question_array = [
     # dependencies, not-dependencies, name, question, default, explanation, example
-    [[],           [],   "lower_case", True,
+    [[], [], "lower_case", True,
      "Should all lower case letters be treated equally?",  # TODO: ask if there are exceptions?
-     "Choose yes if the concrete lower case letter present does not have a crucial impact on the meaning.",
-     "'face' and 'tree' will be treated equally since both consist of four lower case letters"],  # 0
-    [[],           [],   "to_lower", True,
+     "Choose yes if the concrete lower case letter present does not have a crucial impact on the meaning.\n" +
+     "For example, 'face' and 'tree' will be treated equally since both consist of four lower case letters"],  # 0
+    [[], [], "to_lower", True,
      "Should an upper case letter be treated as its lower case variant?",
-     "Choose yes if the capitalization does not have a crucial impact on the meaning.",
-     "'Painting' and 'painting' will be treated equally since the only difference is the capitalization of 'p'"],  # 1
-    [[],           [1],  "upper_case", True,
+     "Choose yes if the capitalization does not have a crucial impact on the meaning.\n" +
+     "For example, 'Painting' and 'painting' will be treated equally since the only difference is the\n" +
+     "capitalization of 'p'"],  # 1
+    [[], [1], "upper_case", True,
      "Should all upper case letters be treated equally?",  # TODO: ask if there are exceptions?
-     "Choose yes if the concrete upper case letter present does not have a crucial impact on the meaning.",
-     "'USA' and 'DDR' will be treated equally since both consist of three upper case letters"],  # 2
-    [[0],          [1],  "lower_sequence", False,
+     "Choose yes if the concrete upper case letter present does not have a crucial impact on the meaning.\n" +
+     "For example, 'USA' and 'DDR' will be treated equally since both consist of three upper case letters"],  # 2
+    [[0], [1], "lower_sequence", False,
      "Should all lower case letter sequences (= lower case word) be treated equally?",
-     "Choose yes if the length of lower case letter sequences does not have a crucial impact on the meaning or if you"
-     + " expect lower case letter sequences of heterogeneous length.",
-     "'red' and 'architecture' will be treated equally since both consist of a sequence of lower case letters"],  # 3
-    [[0, 1],       [],   "sequence", False,
+     "Choose yes if the length of lower case letter sequences does not have a crucial impact on the meaning or if\n" +
+     "you expect lower case letter sequences of heterogeneous length.\n" +
+     "For example, 'red' and 'architecture' will be treated equally since both consist of a sequence of lower case\n" +
+     "letters"],  # 3
+    [[0, 1], [], "sequence", False,
      "Should all letter sequences be treated equally?",
-     "Choose yes if the length of lower, upper or mixed case letter sequences does not have a crucial impact on the"
-     + " meaning or if you expect such letter sequences of heterogeneous length.",
-     "'Portrait' and 'USA' will be treated equally since both consist of a sequence of lower, upper or mixed case"
-     + " letters"],  # 4
-    [[2],          [1],  "upper_sequence", False,
+     "Choose yes if the length of lower, upper or mixed case letter sequences does not have a crucial impact on the\n" +
+     "meaning or if you expect such letter sequences of heterogeneous length.\n" +
+     "For example, 'Portrait' and 'USA' will be treated equally since both consist of a sequence of lower, upper\n" +
+     "or mixed case letters"],  # 4
+    [[2], [1], "upper_sequence", False,
      "Should all upper case letter sequences be treated equally?",
-     "Choose yes if the length of upper case letter sequences does not have a crucial impact on the meaning or if you"
-     + " expect upper case letter sequences of heterogeneous length.",
-     "'EU' and 'USA' will be treated equally since both consist of a sequence of upper case letters"],  # 5
-    [[0, 3],       [1],  "words", False,
+     "Choose yes if the length of upper case letter sequences does not have a crucial impact on the meaning or if\n" +
+     "you expect upper case letter sequences of heterogeneous length.\n" +
+     "For example, 'EU' and 'USA' will be treated equally since both consist of a sequence of upper case letters"],  # 5
+    [[0, 3], [1], "words", False,
      "Should all lower and upper case words be treated equally?",
-     "Choose yes if the capitalization of the first letter of a word does not have a crucial impact on the meaning.",
-     "'Portrait' and 'child' will be treated equally since both are words"],  # 6
-    [[0, 3, 6],    [1],  "word_sequence", False,
+     "Choose yes if the capitalization of the first letter of a word does not have a crucial impact on the meaning.\n" +
+     "For example, 'Portrait' and 'child' will be treated equally since both are words"],  # 6
+    [[0, 3, 6], [1], "word_sequence", False,
      "Should all sequences of lower or upper case words seperated by a blank space be treated equally?",
-     "Choose yes if the length of sequences of lower or upper case words does not have a crucial impact on the meaning"
-     + " or if you expect sequences of lower or upper case words of heterogeneous length.",
-     "'in Marburg' and 'brother or father' will be treated equally since both are sequences of words seperated by a "
-     + "blank space"],  # 7
-    [[0, 1, 4],    [],   "sequence_sequence", False,
-     "Should all sequences of lower, upper or mixed case letter sequences seperated by a blank space be treated"
-     + " equally?",
-     "Choose yes if the length of sequences of lower, upper or mixed case letter sequences does not have a crucial"
-     + " impact on the meaning or if you expect sequences of lower, upper or mixed case letter sequences of"
-     + " heterogeneous length.",
-     "'in USA' and 'around or in Marburg' will be treated equally since both are sequences of letter sequences"
-     + " seperated by a blank"],  # 8
+     "Choose yes if the length of sequences of lower or upper case words does not have a crucial impact on the\n" +
+     "meaning or if you expect sequences of lower or upper case words of heterogeneous length.\n" +
+     "For example, 'in Marburg' and 'brother or father' will be treated equally since both are sequences of words\n" +
+     "seperated by a blank space"],  # 7
+    [[0, 1, 4], [], "sequence_sequence", False,
+     "Should all sequences of lower, upper or mixed case letter sequences seperated by a blank space be treated equally?",
+     "Choose yes if the length of sequences of lower, upper or mixed case letter sequences does not have\n" +
+     "a crucial impact on the meaning or if you expect sequences of lower, upper or mixed case letter sequences of\n" +
+     "heterogeneous length.\n" +
+     "For example, 'in USA' and 'around or in Marburg' will be treated equally since both are sequences of letter\n" +
+     "sequences seperated by a blank"],  # 8
 
-    [[],           [],   "digits", True,
+    [[], [], "digits", True,
      "Should all digits be treated equally?",  # TODO: ask if there are exceptions
-     "Choose yes if the concrete digit does not have a crucial impact on the meaning.",
-     "'1' and '2' will be treated equally since both are digits"],  # 9
-    [[9],          [],   "int", False,
+     "Choose yes if the concrete digit does not have a crucial impact on the meaning.\n" +
+     "For example, '1' and '2' will be treated equally since both are digits"],  # 9
+    [[9], [], "int", False,
      "Should all digit sequences be treated equally?",
-     "Choose yes if the length of digit sequences does not have a crucial impact on the meaning or if you expect digit"
-     + " sequences of heterogeneous length.",
-     "'1' and '1024' will be treated equally since both are digit sequences"],  # 10
-    [[9, 10],      [],   "float", False,
+     "Choose yes if the length of digit sequences does not have a crucial impact on the meaning or if you expect\n" +
+     "digit sequences of heterogeneous length.\n" +
+     "For example, '1' and '1024' will be treated equally since both are digit sequences"],  # 10
+    [[9, 10], [], "float", False,
      "Should all pairs of digit sequences separated by a comma be treated equally?",
-     "Choose yes if the length of digit sequences preceding and following a comma does not have a crucial impact on the"
-     + " meaning or if you expect pairs of digit sequences separated by a comma of heterogeneous length.",
-     "'118,43' and '0,5' will be treated equally since both consist of two digit sequences separated by a comma"],  # 11
+     "Choose yes if the length of digit sequences preceding and following a comma does not have a crucial impact on\n" +
+     "the meaning or if you expect pairs of digit sequences separated by a comma of heterogeneous length.\n" +
+     "For example, '118,43' and '0,5' will be treated equally since both consist of two digit sequences separated\n" +
+     "by a comma"],# 11
 
-    [[],           [],   "specials", False,
+    [[], [], "specials", False,
      "Should all characters that are not letters or digits be treated equally?",
-     "Choose yes if the concrete character does not have a crucial impact on the meaning.",
-     "'/' and '?' will be treated equally since both are neither a letter nor a digit"],  # 12
-    [[],           [12], "punctuation", False,
+     "Choose yes if the concrete character does not have a crucial impact on the meaning.\n" +
+     "For example, '/' and '?' will be treated equally since both are neither a letter nor a digit"],  # 12
+    [[], [12], "punctuation", False,
      "Should all punctuation marks be treated equally?",
-     "Choose yes if the concrete punctuation mark does not have a crucial impact on the meaning.",
-     "'.' and '?' will be treated equally since both are punctuation marks"],  # 13
-    [[],           [12], "brackets", False,
+     "Choose yes if the concrete punctuation mark does not have a crucial impact on the meaning.\n" +
+     "For example, '.' and '?' will be treated equally since both are punctuation marks"],  # 13
+    [[], [12], "brackets", False,
      "Should all bracket characters be treated equally?",
-     "Choose yes if the concrete bracket character does not have a crucial impact on the meaning.",
-     "'{' and )'' will be treated equally since both are bracket characters"],  # 14
-    [[],           [12], "math_characters", False,
+     "Choose yes if the concrete bracket character does not have a crucial impact on the meaning.\n" +
+     "For example, '{' and )'' will be treated equally since both are bracket characters"],  # 14
+    [[], [12], "math_characters", False,
      "Should all mathematical characters (i.e. '+','-','*','/', '%', '=', '<', '>', '&', '|') be treated equally?",
-     "Choose yes if the concrete mathematical character does not have a crucial impact on the meaning.",
-     "'+' and '&' will be treated equally since both are mathematical characters"],  # 15
-    [[],           [12], "quotation_marks", False,
+     "Choose yes if the concrete mathematical character does not have a crucial impact on the meaning.\n" +
+     "For example, '+' and '&' will be treated equally since both are mathematical characters"],  # 15
+    [[], [12], "quotation_marks", False,
      "Should all quotation marks be treated equally?",
-     "Choose yes if the concrete quotation mark does not have a crucial impact on the meaning.",
-     "'\"'' and ''' will be treated equally since both are quotation marks"],  # 16
-    [[],           [12], "special_rest", False,
+     "Choose yes if the concrete quotation mark does not have a crucial impact on the meaning.\n" +
+     "For example, '\"'' and ''' will be treated equally since both are quotation marks"],  # 16
+    [[], [12], "special_rest", False,
      "Should all other special characters be treated equally?",
-     "Choose yes if the concrete specical character does not have a crucial impact on the meaning.",
-     "'$' and 'µ' will be treated equally since both are specical characters"]   # 17
+     "Choose yes if the concrete specical character does not have a crucial impact on the meaning.\n" +
+     "For example, '$' and 'µ' will be treated equally since both are specical characters"] # 17
 ]
 
 dictionary = [
---
+++ DataValueClustering/gui/QuestionnaireInput.py
@@ -0,0 +1,138 @@
+from tkinter import Label, Checkbutton, Button, Tk, IntVar, StringVar
+
+import numpy as np
+
+from clustering_algorithms import algorithm_selection
+from compression import compression
+from gui_distances.ToolTip import CreateToolTip
+
+
+class QuestionnaireInput:
+
+    # config: [dependencies, not-dependencies, name, default, question, notes?]
+
+    def __init__(self, title, config):
+        # Parameters
+        self.config = np.array(config, dtype=object)
+        self.n = len(config)
+        self.m = len(self.config[0])
+        self.check_config()
+
+        # Variables
+        self.config_dep = self.config[:, 0]
+        self.config_notdep = self.config[:, 1]
+        self.config_name = self.config[:, 2]
+        self.config_default = self.config[:, 3]
+        self.config_question = self.config[:, 4]
+        self.config_notes = self.config[:, 5] if self.m > 5 else None
+
+        # GUI
+        self.root = Tk()
+        self.root.title(title)
+        self.root.config(bg='white')
+        self.labels = np.empty(self.n, dtype=Label)
+        self.checks = np.empty(self.n, dtype=Checkbutton)
+        self.answers = np.empty(self.n, dtype=IntVar)
+        self.questions = np.empty(self.n, dtype=StringVar)
+
+        # Initialize Widgets
+        for i, question in enumerate(self.questions):
+            self.questions[i] = StringVar()
+            self.questions[i].set(self.config_question[i])
+            self.labels[i] = Label(self.root, width=100, anchor='w', textvariable=self.questions[i], text=question, bg='white')
+            if self.m > 5:
+                message = str(self.config_notes[i])
+                CreateToolTip(self.labels[i], text=message)
+                # self.labels[i].bind("<Enter>", (lambda event, i2=i: self.on_label_enter(event, i2)))
+                # self.labels[i].bind("<Leave>", (lambda event, i2=i: self.on_label_leave(event, i2)))
+
+            self.answers[i] = IntVar()
+            self.answers[i].set(int(self.config_default[i]))
+            self.checks[i] = Checkbutton(self.root, variable=self.answers[i], command=self.update_visibility, bg='white')
+
+        self.visible = np.full(self.n, False)
+        self.update_visibility()
+
+        self.button = Button(self.root, text='OK', command=self.close, bg='white')
+        self.button.grid(sticky='nsew', row=self.n + 10, column=0, columnspan=2)
+
+        self.root.mainloop()
+
+    def update_visibility(self):
+        for i in range(self.n):
+            is_visible = self.visible[i]
+            should_visible = self.should_be_visible(i)
+            if not is_visible and should_visible:
+                self.labels[i].grid(row=i + 5, column=0, sticky='w')
+                self.checks[i].grid(row=i + 5, column=1)
+            if is_visible and not should_visible:
+                self.labels[i].grid_forget()
+                self.checks[i].grid_forget()
+                self.answers[i].set(False)
+            self.visible[i] = should_visible
+
+    def should_be_visible(self, i):
+        """ test if question i should be visible"""
+        bool = True
+        for d in self.config_dep[i]:
+            bool &= self.answers[d].get() and self.visible[d]
+        for nd in self.config_notdep[i]:
+            bool &= not self.answers[nd].get()  # or not self.visible[nd]
+        return bool
+
+    def check_questions(self):
+        pass
+
+    def get(self):
+        answers = []
+        for i, v in enumerate(self.answers):
+            answers.append(v.get() == 1)
+        return answers
+
+    def close(self):
+        self.root.destroy()
+
+    def check_config(self):
+        assert self.n > 0
+        assert 5 <= self.m <= 6
+        for i in range(self.n):
+            assert self.m == len(self.config[i])
+            assert isinstance(self.config[i, 0], list)
+            assert isinstance(self.config[i, 1], list)
+            assert isinstance(self.config[i, 2], str)
+            assert isinstance(self.config[i, 3], bool)
+            assert isinstance(self.config[i, 4], str)
+            if self.m > 5:
+                assert isinstance(self.config[i, 5], str)
+
+            assert not self.config[i, 0] or max(self.config[i, 0]) < i
+            assert not self.config[i, 1] or max(self.config[i, 1]) < i
+
+    # def on_label_enter(self, event, i):
+    #     message = str(self.config_notes[i])
+    #
+    #
+    #     print("on index " + str(i) + " show message: " + message)
+    #
+    # def on_label_leave(self, event, i):
+    #     print("hide")
+
+
+if __name__ == '__main__':
+    title = "myQuestions"
+    q_config = np.array(
+        # 0              1                 2     3        4         5             6
+        # [dependencies, not-dependencies, name, default, question, explanation?, example?]
+        [[[], [], "name0", False, "question0?"],
+         [[], [], "name1", True, "question1?"],
+         [[0], [], "name2", False, "question2?"],
+         [[1], [], "name3", True, "question3?"],
+         [[], [3], "name4", False, "question4?"],
+         [[3], [], "name5", True, "question5?"],
+         [[0], [1], "name6", False, "question6?"]],
+        dtype=object)
+
+    q_config2 = compression.question_array
+    q_config3 = algorithm_selection.question_array
+
+    print(QuestionnaireInput(title, q_config3).get())
