8a92e9028dacd78a0d7badaae18c0433977aa052
kesperar <arno.kesper@uni-marburg.de>
2021-01-20 13:03:53 +0100

    Add regex to information in canvas. Modify Gui distances. Catch Nontype errors on drag.

--- DataValueClustering/gui_distances/BlobInput.py
+++ DataValueClustering/gui_distances/BlobInput.py
@@ -53,11 +53,13 @@ class BlobInput:
         self.max_self_dif = 5
         self.distance_factor = 1 / self.image_sizes  # self.max_distance / self.diagonal
         self.size_factor = 1.  # 2 / self.image_sizes  # self.max_distance / self.diagonal / 0.62
-        self.distance_threshold = 0. # self.diagonal / 20
+        self.distance_threshold = 0.  # self.diagonal / 20
         self.coordinates = create_coordinates(self.x, self.y, self.labels)  # array: [label, x, y, size]
 
         """Canvas"""
-        self.canvas = Canvas(width=self.w, height=37*self.h//40, highlightbackground="black")  # , background="alice blue")
+        self.gui_spacing = 5
+        self.canvas = Canvas(width=self.w - self.gui_spacing, height=17 * self.h // 18 - 2 * self.gui_spacing,
+                             highlightbackground="black")  # , background="alice blue")
         self.canvas.place(anchor='nw', x=0, y=0)
 
         # garbage collector defense mechanism
@@ -86,12 +88,13 @@ class BlobInput:
         """Build Blobs"""
         self.blobs = np.empty(len(chars_labels), dtype=Blob)
         for i, c in reversed(list(enumerate(self.coordinates))):
-            self.blobs[i] = Blob(self, self.labels[i], c[0], c[1], self.image_sizes, self.resizable[i], self.chars_info[i])
+            self.blobs[i] = Blob(self, label=self.labels[i], x=c[0], y=c[1], size=self.image_sizes,
+                                 resizable=self.resizable[i], regex=self.regex[i], info=self.chars_info[i])
 
         """Buttons"""
-        self.button_distance = self.h // 100
+
         self.button_h = self.y // 9
-        self.button_w = self.x - 2 * self.button_distance
+        self.button_w = self.x - 2 * self.gui_spacing
 
         self.button_image_restart = Image.open("blob_images\\button_restart.png")
         self.button_image_restart = self.button_image_restart.resize((self.button_w, self.button_h), Image.ANTIALIAS)
@@ -101,22 +104,24 @@ class BlobInput:
         self.button_image_ok = ImageTk.PhotoImage(self.button_image_ok)
 
         self.button_restart = Button(self.root, text='Restart', command=self.restart,
-                                     width=self.button_w, height=self.button_h,
-                                     bg='black', border=0, image=self.button_image_restart)  # background='brown2',
-        self.button_restart.place(anchor='center', x=self.x // 2, y=self.h - self.button_h // 2 - self.button_distance)
+                                     width=self.button_w, height=self.button_h, bg='black', border=0,
+                                     image=self.button_image_restart)
+        self.button_restart.place(anchor='center', x=self.x // 2, y=self.h - self.button_h // 2 - self.gui_spacing)
 
         self.button_ok = Button(self.root, text='OK', command=self.close,
-                                width=self.button_w, height=self.button_h,
-                                bg='black', border=0, image=self.button_image_ok)  # background='lime green',
-        self.button_ok.place(anchor='center', x=3 * self.x // 2, y=self.h - self.button_h // 2 - self.button_distance)
+                                width=self.button_w, height=self.button_h, bg='black', border=0,
+                                image=self.button_image_ok)
+        self.button_ok.place(anchor='center', x=3 * self.x // 2, y=self.h - self.button_h // 2 - self.gui_spacing)
         self.root.mainloop()
 
     def canvas_blob_info(self, event):
+        """On mouse over show Information of Blob in Canvas"""
         blob = self.find_nearest_blob(event.x, event.y)
         if isinstance(blob, Blob):
             text = blob.info
-            text += "\ndist: " + str(blob.get_distance(event.x, event.y)) + ""
-            text += "\nsize: " + str(blob.get_size() * self.size_factor) + "" if blob.resizable else ""
+            # text += "\ndist.: " + str(blob.get_distance(event.x, event.y)) + ""
+            text += "\nregex: " + blob.regex
+            text += "\nsize : " + str(blob.get_size() * self.size_factor) + "" if blob.resizable else ""
             self.canvas.itemconfigure(self.canvas.text, text=text)
         else:
             self.canvas.itemconfigure(self.canvas.text, text="")
@@ -126,7 +131,7 @@ class BlobInput:
         min_blob, min_distance = (None, self.distance_threshold)
         # min_blob, min_distance = (self.blobs[0], self.blobs[0].get_distance(x, y))
         for i, blob in enumerate(self.blobs):
-            d = (blob.get_distance(x, y) - blob.size/2) / blob.size
+            d = (blob.get_distance(x, y) - blob.size / 2) / blob.size
             if d < min_distance:
                 min_blob, min_distance = blob, d
         return min_blob
@@ -135,7 +140,8 @@ class BlobInput:
         """Beginning drag of an object"""
         # record the item and its location
         nearest = self.find_nearest_blob(event.x, event.y)
-        nearest.lift()
+        if isinstance(nearest, Blob):
+            nearest.lift()
         self._drag_data["item"] = nearest
         self._drag_data["item_last"] = None
         self._drag_data["x"] = event.x
@@ -155,7 +161,8 @@ class BlobInput:
         delta_x = event.x - self._drag_data["x"]
         delta_y = event.y - self._drag_data["y"]
         # move the blob the appropriate amount
-        self._drag_data["item"].move(delta_x, delta_y)
+        if isinstance(self._drag_data["item"], Blob):
+            self._drag_data["item"].move(delta_x, delta_y)
         # record the new position
         self._drag_data["x"] = event.x
         self._drag_data["y"] = event.y
@@ -181,26 +188,26 @@ class BlobInput:
             return {}
         distance_map = {(()): 1.}
 
-        for i, l in enumerate(self.labels):
-            distance_map[i] = self.regex[i]
+        for i, blob_i in enumerate(self.blobs):
+            distance_map[i] = blob_i.regex
 
-        for i, label_i in enumerate(self.labels):
-            for j, label_j in enumerate(self.labels):
-                if i == j:
-                    # (i,j), i==j -> blobsize(i)
-                    distance_map[(i, i)] = round((self.blobs[i].get_size() * self.size_factor), 2)
-                else:
-                    # (i,j), i!=j -> distance ( blob(i), blob(j) )
+            for j, blob_j in enumerate(self.blobs):
+                if i == j:  # (i,j), i==j -> blobsize(i)
+                    distance_map[(i, i)] = round((blob_i.get_size() * self.size_factor), 2)
+
+                else:  # (i,j), i!=j -> distance ( blob(i), blob(j) )
                     distance_map[(i, j)] = round(
-                        (self.blobs[i].get_distance(blob=self.blobs[j]) * self.distance_factor), 2)
+                        (blob_i.get_distance(blob=blob_j) * self.distance_factor), 2)
 
         return distance_map
 
     def close(self, canceled=False):
+        """Close Tk Window"""
         self.canceled = canceled
         self.root.destroy()
 
     def restart(self):
+        """Reopen the window, inclusive positions and sizes."""
         chars_labels = self.chars_labels
         self.root.destroy()
         self.__init__(chars_labels)
@@ -208,14 +215,13 @@ class BlobInput:
 
 if __name__ == '__main__':
     min_blobs = [False, False, False, False, False, False, False, False, False,
-        False, False, False,
-        True, False, False, False, False, False]
+                 False, False, False,
+                 True, False, False, False, False, False]
     min_blob_config = get_blob_configuration(min_blobs)
     print_cost_matrix(BlobInput(min_blob_config).get())
 
     # max_blobs = [False, False, True, True, True, True, True, True, True,
-    #     True, True, True,
-    #     False, True, True, True, True, True]
+    #              True, True, True,
+    #              False, True, True, True, True, True]
     # max_blob_config = get_blob_configuration(max_blobs)
     # print(str(BlobInput(max_blob_config).get()))
-
