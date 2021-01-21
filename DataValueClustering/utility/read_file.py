30b2b9a58a411c3c0fdbcc21862ab21d58b76320
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-01-21 12:45:33 +0100

    Change encoding for file reading

--- DataValueClustering/utility/read_file.py
+++ DataValueClustering/utility/read_file.py
@@ -1,5 +1,5 @@
 def read_data_values_from_file(path):
-    with open(path) as f:
+    with open(path, encoding='UTF-8') as f:
         values = f.read().splitlines()
     assert len(values) > 0
     return values
