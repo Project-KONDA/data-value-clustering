8f01a23a1d14ab2d4a662fddf83e6b98d4ddca12
kesperar <arno.kesper@uni-marburg.de>
2021-01-21 18:19:09 +0100

    Add basic main GUI.

--- DataValueClustering/gui/main.py
+++ DataValueClustering/gui/main.py
@@ -1,20 +1,37 @@
-from tkinter import *
 from clustering.clustering import cluster
 from gui.cluster_algorithms_gui import cluster_algorithms
 from gui.compression_functions_gui import compression_functions
 from gui.distance_functions_gui import distance_functions
+from gui.DropdownInput import input_dropdown
+from utility.read_file import get_sources_in_experiment_data_directory
+
+l_data = get_sources_in_experiment_data_directory()
+l_compressions = compression_functions
+l_distances = distance_functions
+l_clusters = cluster_algorithms
 
 
-def execute(a, b, c, d):
+def execute(data_i, compression_i, distance_i, cluster_i):
+
+
+    # DATA
+    # TODO: choose data
+    data = l_data[data_i, 1]()[0:1000]
+
     # CONFIGURATION
     # configuration compression?
-    compression_f = compression_functions[compression_index, 1]
+    compression_f = l_compressions[compression_i, 1]
 
     # configuration distance
-    distance_f = distance_functions[distance_index, 1]()
+    distance_f = l_distances[distance_i, 1]()
 
     # configuration cluster
-    cluster_f = cluster_algorithms[cluster_index, 1]()
+    cluster_f = cluster_algorithms[cluster_i, 1]()
+
+    print("Execute ... [", "Data:", l_data[data_i, 0],
+          "Compression:", l_compressions[compression_i, 1],
+          "Distance:", l_distances[distance_i, 0],
+          "Cluster:", cluster_algorithms[cluster_i, 0], "]")
 
     # EXECUTION
     cluster_list, noise = cluster(data, compression_f, distance_f, cluster_f)
@@ -25,33 +42,22 @@ def execute(a, b, c, d):
 
     # CLUSTER VALIDATION
     # TODO
+
     # SUGGEST DATA ENHANCEMENTS
     # TODO
 
 
 if __name__ == '__main__':
-    # GUI
-    root = Tk()
-    root.title("Value Clustering")
-    root.geometry("570x110")
+    title = "Hello"
+    labels = ["data", "compression", "distance", "cluster"]
+    matrix = [
+        list(l_data[:, 0]),
+        list(l_compressions[:, 0]),
+        list(l_distances[:, 0]),
+        list(l_clusters[:, 0])
+    ]
+    answers, answer_indexes = input_dropdown(title, labels, matrix)
+    assert (len(answer_indexes) == 4)
+    [data_index, compression_index, distance_index, cluster_index] = answer_indexes
 
-    # INPUT
-    # TODO
-    # choose data -> query?
-    data = ["a", "b", "0"]
-
-    # choose compression
-    compression_index = 0
-
-    # choose distance function
-    distance_index = 3
-
-    # choose cluster algorithm
-    cluster_index = 0
-
-    button = Button(root, text='OK', width=75,
-                    command=lambda: execute(data, compression_index, distance_index, cluster_index))
-    button.grid(row=6, column=0, columnspan=5)
-
-    # GUI
-    root.mainloop()
+    execute(data_index, compression_index, distance_index, cluster_index)
