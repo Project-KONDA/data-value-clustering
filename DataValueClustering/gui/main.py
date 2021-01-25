563039513e8c1c1297bc1e6439bbf176a4f51fbe
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-01-25 07:54:08 +0100

    Minor corrections in main

--- DataValueClustering/gui/main.py
+++ DataValueClustering/gui/main.py
@@ -1,9 +1,9 @@
-from clustering.clustering import cluster
+from main.clustering import cluster
 from gui.cluster_algorithms_gui import cluster_algorithms
 from gui.compression_functions_gui import compression_functions
 from gui.distance_functions_gui import distance_functions
 from gui.DropdownInput import input_dropdown
-from utility.read_file import get_sources_in_experiment_data_directory
+from data_extraction.read_file import get_sources_in_experiment_data_directory
 
 l_data = get_sources_in_experiment_data_directory()
 l_compressions = compression_functions
@@ -16,7 +16,9 @@ def execute(data_i, compression_i, distance_i, cluster_i):
 
     # DATA
     # TODO: choose data
-    data = l_data[data_i, 1]()[0:1000]
+    data = l_data[data_i, 1]()
+    if len(data) > 1000:
+        data = data[0:1000]
 
     # CONFIGURATION
     # configuration compression?
@@ -29,7 +31,7 @@ def execute(data_i, compression_i, distance_i, cluster_i):
     cluster_f = cluster_algorithms[cluster_i, 1]()
 
     print("Execute ... [", "Data:", l_data[data_i, 0],
-          "Compression:", l_compressions[compression_i, 1],
+          "Compression:", l_compressions[compression_i, 0],
           "Distance:", l_distances[distance_i, 0],
           "Cluster:", cluster_algorithms[cluster_i, 0], "]")
 
