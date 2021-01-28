b2fd1add1f7c51aaff1ac1c15bcb678518d56c78
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-01-28 09:06:32 +0100

    Add TODO

--- DataValueClustering/gui_clustering/clustering_questionnaire.py
+++ DataValueClustering/gui_clustering/clustering_questionnaire.py
@@ -4,5 +4,5 @@ from gui_clustering.clustering_questions import clustering_question_array
 
 def cluster_suggest():
     predefined_answers = None  # TODO
-    answers, cluster_f = input_questionnaire_clustering(clustering_question_array, predefined_answers)  # cluster_f function in this module
-    return cluster_f(answers)
+    answers, cluster_f = input_questionnaire_clustering(clustering_question_array, predefined_answers)
+    return cluster_f(answers)  # TODO: call directly or return function to main?
