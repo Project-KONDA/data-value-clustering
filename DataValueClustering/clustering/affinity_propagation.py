4e00a3fa3e16a415c19ab2f440a3dec1037b8cfe
kesperar <arno.kesper@uni-marburg.de>
2021-02-04 13:05:03 +0100

    Partial parameter parametrisation.

--- DataValueClustering/clustering/affinity_propagation.py
+++ DataValueClustering/clustering/affinity_propagation.py
@@ -20,19 +20,21 @@ def affinity(affinity_matrix, values, damping=0.5, max_iter=200, convergence_ite
 
 
 def affinity_args(damping, max_iter, convergence_iter, copy, preference):
-    return lambda distance_matrix_map, values: affinity(distance_matrix_map["affinity_matrix"], values, damping, max_iter, convergence_iter,
-                            copy, preference)
+    return lambda distance_matrix_map, values: affinity(distance_matrix_map["affinity_matrix"], values, damping,
+                                                        max_iter, convergence_iter, copy, preference)
 
 
 def affinity_damping_config():
     # float
     name = DAMPING
-    explanation = "Higher values will yield less clusters."
+    explanation = "Extent to which the current value is maintained relative to incoming values:\n"
+    explanation += "Higher values will yield less clusters."
     min_damping = 0.5
     max_damping = 0.99
     suggestion_value = 0.5
     resolution = 0.01
-    return name, explanation, min_damping, max_damping, suggestion_value, resolution
+    deactivatable = False
+    return name, explanation, min_damping, max_damping, suggestion_value, resolution, deactivatable
 
 
 def affinity_preference_config(affinity_matrix):
@@ -40,9 +42,9 @@ def affinity_preference_config(affinity_matrix):
     # it is advised to start with a preference equal to the median of the input similarities (= default)
     name = PREFERENCE
     explanation = "Lower values will yield less clusters."
-    min_preference = 0.0  # TODO
-    max_preference = 100.0  # TODO
-    suggestion_value = float(np.median(affinity_matrix))  # = default
+    min_preference = 0.0
+    max_preference = 100.0
+    suggestion_value = float(np.median(affinity_matrix))
     resolution = 0.01
     deactivatable = True
     return name, explanation, min_preference, max_preference, suggestion_value, resolution, deactivatable
@@ -50,37 +52,31 @@ def affinity_preference_config(affinity_matrix):
 
 def affinity_max_iter_config(answers):
     # int
-    name = MAX_ITER  # TODO
-    explanation = ""  # TODO
-    mini = 0  # TODO
-    maxi = 2  # TODO
-    default = 1  # TODO
-    resolution = 1  # TODO
-    deactivatable = True  # TODO
+    name = MAX_ITER
+    explanation = "The algorithm runs multiple times and improves with every iteration.\n"
+    explanation += "This parameter determines the maximum number of iterations the algorithm will perform.\n"
+    explanation += "The default value is 200."
+    mini = 1
+    maxi = 500
+    default = 200
+    resolution = 1
+    deactivatable = False  # TODO
     return name, explanation, mini, maxi, default, resolution, deactivatable
 
 
 def affinity_convergence_iter_config():
     # int
     # increase for more clusters?
-    name = CONVERGENCE_ITER  # TODO
-    explanation = ""  # TODO
-    mini = 0  # TODO
-    maxi = 2  # TODO
-    default = 1  # TODO
-    resolution = 1  # TODO
+    name = CONVERGENCE_ITER
+    explanation = "Another termination condition is, when a specific amount of iterations there is no difference.\n"
+    explanation += "This parameter determines, how much iterations are needed to terminate the algorithm.\n"
+    explanation += "The default value is 15"
+    mini = 2
+    maxi = 100
+    default = 15
+    resolution = 1
     deactivatable = True  # TODO
     return name, explanation, mini, maxi, default, resolution, deactivatable
-
-
-def affinity_copy_config():
-    # bool
-    # TODO: remove?
-    name = COPY  # TODO
-    explanation = ""  # TODO
-    default = True  # TODO
-    deactivatable = True  # TODO
-    return name, explanation, default, deactivatable
 
 
 if __name__ == '__main__':
--- DataValueClustering/clustering/spectral.py
+++ DataValueClustering/clustering/spectral.py
@@ -20,8 +20,8 @@ def spectral(affinity_matrix, values, n_clusters=8, eigen_solver=None, n_compone
 
 
 def spectral_args(n_clusters, eigen_solver, n_components, n_init, eigen_tol, assign_labels):
-    return lambda distance_matrix_map, values: spectral(distance_matrix_map["affinity_matrix"], values, n_clusters, eigen_solver,
-                            n_components, n_init, eigen_tol, assign_labels)
+    return lambda distance_matrix_map, values: spectral(distance_matrix_map["affinity_matrix"], values, n_clusters,
+                                                        eigen_solver, n_components, n_init, eigen_tol, assign_labels)
 
 
 def spectral_n_clusters_config(no_values):
@@ -30,43 +30,45 @@ def spectral_n_clusters_config(no_values):
     explanation = "Maximum number of clusters created. Higher values will yield more clusters."
     min_n_clusters = 2
     max_n_clusters = no_values
-    suggestion_value = min(7, no_values / 2)
-    return name, explanation, min_n_clusters, max_n_clusters, suggestion_value
+    suggestion_value = min(7, no_values // 2)
+    deactivatable = False
+    return name, explanation, min_n_clusters, max_n_clusters, suggestion_value, deactivatable
 
 
 def spectral_eigen_solver_config():
     # enum
     name = EIGEN_SOLVER  # TODO
-    explanation = ""  # TODO
+    explanation = "Eigenvalue decomposition strategy."  # TODO
     options = [
-        ["arpack", ""],
-        ["lobpcg", ""],
-        ["amg", ""]
+        ['arpack', "Default Value."],
+        ['lobpcg', ""],
+        ['amg', "Faster, but partially instable."]
     ]  # TODO
-    suggestions = [""]  # TODO
-    deactivatable = True
+    suggestions = ['arpack']  # TODO
+    deactivatable = False  # If None 'arpack' is used
     return name, explanation, options, suggestions, deactivatable
 
 
-def spectral_n_components_config():
+def spectral_n_components_config(no_values):
     # int
     name = N_COMPONENTS  # TODO
     explanation = ""  # TODO
     mini = 0  # TODO
     maxi = 2  # TODO
-    default = 1  # TODO
+    default = min(7, no_values // 2)  # TODO : n.clusters
     resolution = 1  # TODO
-    deactivatable = True
+    deactivatable = False
     return name, explanation, mini, maxi, default, resolution, deactivatable
 
 
-def spectral_n_init_config():
+def spectral_n_init_config(no_values):
     # int
     name = N_INIT  # TODO
-    explanation = ""  # TODO
-    mini = 0  # TODO
-    maxi = 2  # TODO
-    default = 1  # TODO
+    explanation = "Number of iterations of the algorithm."
+    explanation += "The final result will be the best result of the single iterations."
+    mini = 1
+    maxi = no_values
+    default = 10  # TODO
     resolution = 1  # TODO
     deactivatable = True
     return name, explanation, mini, maxi, default, resolution, deactivatable
@@ -74,14 +76,14 @@ def spectral_n_init_config():
 
 def spectral_eigen_tol_config():
     # float
-    # when eigen_solver='arpack'
+    # when eigen_solver='arpack' = default
     name = EIGEN_TOL  # TODO
     explanation = ""  # TODO
     mini = 0.  # TODO
     maxi = 2.  # TODO
     default = 1.  # TODO
     resolution = 0.01
-    deactivatable = True
+    deactivatable = False
     return name, explanation, mini, maxi, default, resolution, deactivatable
 
 
@@ -90,8 +92,9 @@ def spectral_assign_labels_config():
     #  k-means can be applied and is a popular choice. But it can also be sensitive to initialization.
     #  Discretization is another approach which is less sensitive to random initialization.
     name = ASSIGN_LABELS  # TODO
-    explanation = ""  # TODO
-    options = [["", ""]]  # TODO
-    suggestions = [""]  # TODO
-    deactivatable = True
+    explanation = "Strategy to use to assign labels in the embedding space."  # TODO
+    options = [['kmeans', ""],
+               ['discretize', ""]]  # TODO
+    suggestions = ['kmeans']  # TODO
+    deactivatable = False
     return name, explanation, options, suggestions, deactivatable
