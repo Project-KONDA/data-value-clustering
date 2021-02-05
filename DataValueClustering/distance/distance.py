370a5ce7667805deb461f2fc9083f7f802a66143
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-02-05 14:26:38 +0100

    Add dict for distance functions

---
+++ DataValueClustering/distance/distance.py
@@ -0,0 +1,9 @@
+from distance import levenshtein_distance, longest_common_subsequence_distance, dice_coefficient_distance, \
+    weighted_levenshtein_distance
+
+distance_functions = {
+    "distance_weighted_levenshtein": lambda cost_map: lambda s1, s2: weighted_levenshtein_distance(cost_map, s1, s2),
+    "distance_levenshtein": lambda cost_map: lambda s1, s2: levenshtein_distance(s1, s2),
+    "longest_common_subsequence_distance": lambda cost_map: lambda s1, s2: longest_common_subsequence_distance(s1, s2),
+    "dice_coefficient_distance": lambda cost_map: lambda s1, s2: dice_coefficient_distance(s1, s2),
+}
