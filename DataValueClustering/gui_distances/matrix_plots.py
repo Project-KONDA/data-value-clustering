afb911eeb4e7f9d587cbe5acfe3b7db4a9e3dc46
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-02-09 14:59:38 +0100

    Move matrix plot functions

---
+++ DataValueClustering/gui_distances/matrix_plots.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def plot_box(condensed_matrix):
+    plt.boxplot(condensed_matrix)
+    plt.show()
+
+
+def plot_histogram(condensed_matrix):
+    n_bins = len(np.array(list(set(condensed_matrix))))
+    plt.hist(condensed_matrix, bins=n_bins)
+    plt.show()
+
+
+def plot_image(matrix):
+    plt.imshow(matrix)
+    plt.colorbar()
+    plt.show()
+
+
+def plot_at_y(condensed_matrix):
+    plt.plot(condensed_matrix, np.zeros_like(condensed_matrix), '.',)
+    plt.show()
