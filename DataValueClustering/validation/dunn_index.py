b161f7e923799aa90be203069b5fb68bce8a9a5a
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-02-18 12:26:46 +0100

    Add dunn_index calculation

--- DataValueClustering/validation/dunn_index.py
+++ DataValueClustering/validation/dunn_index.py
@@ -1,6 +1,9 @@
+import numpy as np
+from validation.intra__inter_cluster_distance import max_intra_cluster_distances, \
+    min_of_min_inter_cluster_distances
+
 
 def dunn_index(clusters, distance_matrix):
-    # clusters = [4 0 3 5 ...]
-    pass
-
-def 
+    min_inter = min_of_min_inter_cluster_distances(clusters, distance_matrix)
+    max_intra = np.amax(max_intra_cluster_distances(clusters, distance_matrix))
+    return min_inter/max_intra
