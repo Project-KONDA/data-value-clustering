80bf2d966500c4f9d4a300e516b955e74c1bbfe8
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-03-12 14:16:15 +0100

    Start evaluation of randomized LIDO title values

---
+++ DataValueClustering/experiments/evaluation/lido_titles_hierarchical_v3_complete_random.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from distance.weighted_levenshtein_distance import get_cost_map
+from experiments.constants import midas_dates, evaluation_exports, lido_measurement_unit, lido_titles, \
+    lido_titles_randomized
+from experiments.evaluation.lido_measurement_unit_expectation import lido_measurement_unit_100000_expectation
+from experiments.evaluation.lido_titles_expectation import lido_titles_1000_expectation
+from experiments.evaluation.lido_titles_expectation_v2 import lido_titles_1000_expectation_v2
+from experiments.evaluation.lido_titles_expectation_v3_complete import lido_titles_1000_expectation_v3_complete
+from experiments.evaluation.midas_dates_expectation import midas_dates_10000_expectation
+from export.ExecutionConfiguration import ExecutionConfigurationFromParams
+
+
+
+
+if __name__ == '__main__':
+    # specify parameters
+
+    # compression
+    compression_answers = [True, True, False, False, True, False, False, False, True,
+                             True, False, False,
+                             False, False, False, False, False, False,
+                             True]
+
+    #distance
+    weight_case = 1
+    regex = ["", "abcdefghijklmnopqrstuvwxyzäöüßáàéèíìóòúù", "ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜÁÀÉÈÍÌÓÒÚÙ", "0123456789", " ", ":,.;!?[]{}()+—-*/%=<>&|\"`´'" , "<rest>"]
+    weights = [
+        #         a      A      1      _      $       r
+        [   0,   500,   500,   900,   800,  2500,    16],  #
+        [ 500,     0,   128,   900,   800,  2500,   500],  # a
+        [ 500,   128,     0,   900,   800,  2500,   500],  # A
+        [ 900,   900,   900,     0,   900,  2500,   900],  # 1
+        [ 800,   800,   800,   900,     0,  2500,   800],  # _
+        [2500,  2500,  2500,  2500,  2500,  3800,  2500],  # $
+        [  16,   500,   500,   900,   800,  2500,    16],  # r
+    ]
+
+    costmap = get_cost_map(weight_case, regex, weights)
+
+    # clustering
+    algorithm = "hierarchical"
+    algorithm_params = [['method', 'complete'], ['n_clusters', None], ['distance_threshold', 12000], ['criterion', 'distance']]
+
+    # initialize
+    object = ExecutionConfigurationFromParams(lido_titles_randomized, 1000, compression_answers, "distance_weighted_levenshtein", algorithm, algorithm_params, costmap,
+                                              lido_titles_1000_expectation_v3_complete)
+
+    # execute
+    object.execute()
+
+    # save
+    object.save(evaluation_exports)
+
+    print("symmetric:", np.allclose(np.array(weights), np.array(weights).T))
+
+    # load
+    # load = load_ExecutionConfiguration("../data/examples/midas_dates_hierarchical_20210215-133033")
+    # load.execute()
