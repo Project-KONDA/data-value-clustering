95939189284f4b2c05a846dcb4acdae52da84444
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-03-25 12:40:53 +0100

    Add file paths to constants

--- DataValueClustering/experiments/constants.py
+++ DataValueClustering/experiments/constants.py
@@ -5,6 +5,7 @@ evaluation_exports = "../experiments/exports/evaluation/"
 midas_dates = "../../data/midas_dates.txt"
 midas_dates_randomized = "../../data/midas_dates_randomized.txt"
 midas_artist_names = "../../data/midas_artist_names.txt"
+midas_aps_artist_names = "../../data/midas_aps_artist_names.txt"
 midas_artist_names_randomized = "../../data/midas_artist_names_randomized.txt"
 midas_measurements = "../../data/midas_measurement.txt"
 
@@ -15,3 +16,6 @@ lido_attribution_qualifier_randomized = "../../data/xlido_attribution_qualifier_
 lido_measurement_unit = "../../data/xlido_measurement_unit.txt"
 lido_measurement_unit_randomized = "../../data/xlido_measurement_unit_randomized.txt"
 lido_repository_names = "../../data/xlido_repository_names.txt"
+
+lido_dates = "../../data/xlido_earliestDate.txt"
+lido_displayDates = "../../data/xlido_displayDate.txt"
