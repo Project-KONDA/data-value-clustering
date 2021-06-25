79fc44d99b045dc6518909349a24e433da7252d1
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-06-25 12:13:24 +0200

    Add read_clusters_compressed()

---
+++ DataValueClustering/export/ReadJSON.py
@@ -0,0 +1,8 @@
+from export.ExecutionConfiguration import load_ExecutionConfiguration
+from gui_center.cluster_representation import fancy_cluster_representation_reverse
+
+
+def read_clusters_compressed(path, values):
+    exe = load_ExecutionConfiguration(path)
+    fancy = exe.cluster_list_compressed
+    return fancy_cluster_representation_reverse(values, fancy)
