4939b52a862ef4492437df623e9c21ee6758d178
kesperar <arno.kesper@uni-marburg.de>
2021-07-23 13:33:49 +0200

    Modify data selection view: Resize elements. Add scrollbar to listview. Add option to delete elements.

--- DataValueClustering/gui_data/select_data.py
+++ DataValueClustering/gui_data/select_data.py
@@ -1,4 +1,7 @@
-from tkinter import Tk, Listbox, Button, Label, END, Scrollbar, Toplevel
+import os
+from tkinter import Tk, Listbox, Button, Label, END, Scrollbar, Toplevel, messagebox
+from tkinter.messagebox import WARNING
+
 import numpy as np
 
 from data_extraction import get_sources_in_experiment_data_directory
@@ -21,26 +24,33 @@ class SelectData:
         # self.root.title("Select Data")
 
         self.result = None
+        self.datalist = list()
         self.canceled = False
 
         self.label_title = Label(self.root, text="Select Data Set", bg="white",
                                  font=('Helvatical bold', 19))
 
-        self.listbox = Listbox(self.root, selectmode="single", width=40, height=10)
+        self.listbox = Listbox(self.root, selectmode="single", width=40, height=20)
+        self.scrollbar = Scrollbar(self.root, command=self.listbox.yview)
+        self.listbox.config(yscrollcommand=self.scrollbar.set)
 
-        self.button_add = Button(self.root, text="+", command=self.add, width=3, state="disabled", bg="grey80")
+        self.button_add = Button(self.root, text="+", command=self.add, width=3, state="disabled")
+        self.button_remove = Button(self.root, text="-", command=self.remove, width=3)
         self.button_ok = Button(self.root, text="OK", command=self.close, width=27)
 
+        self.label_title.grid(sticky='nswe', row=1, column=1, columnspan=4)
+        self.listbox.grid(sticky='nswe', row=3, column=1, columnspan=3)
+        self.scrollbar.grid(sticky='nse', row=3, column=4)
         self.button_add.grid(sticky='nswe', row=5, column=1, columnspan=1)
-        self.label_title.grid(sticky='nswe', row=1, column=1, columnspan=2)
-        self.listbox.grid(sticky='nswe', row=3, column=1, columnspan=2)
-        self.button_ok.grid(sticky='nswe', row=5, column=2, columnspan=1)
+        self.button_remove.grid(sticky='nswe', row=5, column=2, columnspan=1)
+        self.button_ok.grid(sticky='nswe', row=5, column=3, columnspan=2)
 
         self.load()
         self.root.protocol("WM_DELETE_WINDOW", self.cancel)
         self.root.mainloop()
 
     def load(self):
+        self.listbox.delete(0, END)
         self.datalist = get_list(self.path)
         for i in self.datalist:
             self.listbox.insert(END, i)
@@ -50,6 +60,29 @@ class SelectData:
 
     def add(self):
         # TODO load
+        self.load()
+
+    def remove(self):
+        index = self.listbox.curselection()
+        if index == ():
+            return
+        text = "Do you really want to delete the following "
+        if len(index) == 1:
+            text += "file?\n" + self.datalist[index]
+        else:
+            print(index)
+            text += "files?"
+            for i in index:
+                text += "\n" + self.datalist[i]
+        if messagebox.askokcancel("Deletion", text, icon=WARNING):
+            for i in index:
+                path = self.datalist[index] + ".txt"
+                if self.path != "":
+                    path += self.path + "\\"
+                if os.path.exists(path):
+                    os.remove(path)
+                else:
+                    print("File", path, "can not be found in order to delete it.")
         self.load()
 
     def cancel(self):
@@ -68,7 +101,6 @@ class SelectData:
 
         self.root.quit()
         self.root.destroy()
-
 
     def get(self):
         if self.canceled:
