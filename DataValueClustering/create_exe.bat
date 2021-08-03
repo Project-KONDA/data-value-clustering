73881c875cd09d826c9cb42072a656676f415d66
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-08-03 15:14:58 +0200

    Add batch file for creating exe file

---
+++ DataValueClustering/create_exe.bat
@@ -0,0 +1,5 @@
+pyinstaller --noconfirm --onedir --windowed --hidden-import "sklearn.neighbors._typedefs" --hidden-import "sklearn.utils._cython_blas" --hidden-import "sklearn.utils._weight_vector" --hidden-import "sklearn.neighbors._quad_tree" --hidden-import "sklearn.tree" --hidden-import "sklearn.tree._utils" --add-data "./data;data/"  "./gui_center/hub.py"
+@ECHO OFF
+ECHO Exe file created in ./dist/hub/
+ECHO Please copy ...\AppData\Local\Programs\Python\Python38\Lib\site-packages\~~vmlite\binding\llvmlite.dll to .dist/hub/ manually
+PAUSE
