8f90fcbec62533067ede392d4509282540ffd305
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-09-23 14:36:39 +0200

    Extract method

--- DataValueClustering/gui_cluster_configuration/k_distance_graph.py
+++ DataValueClustering/gui_cluster_configuration/k_distance_graph.py
@@ -4,14 +4,7 @@ from matplotlib import pyplot as plt
 
 
 def show_k_distance_graph(distance_matrix, k):
-    distances = np.empty(len(distance_matrix), dtype=float)
-    for i in range(len(distance_matrix)):
-        d = distance_matrix[i, :]
-        sorted = np.sort(d)
-        # distances[i] = sum(sorted[0:k-1])/k
-        distances[i] = sum(sorted[1:k + 1]) / k
-        # distances[i] = sorted[k+1]
-    distances_sorted = np.sort(distances)
+    distances_sorted = get_sorted_k_distances(distance_matrix, k)
     plt.plot(distances_sorted)
 
     plt.title('K-Distance Graph')
@@ -25,3 +18,15 @@ def show_k_distance_graph(distance_matrix, k):
     print("showing k_distance graph ...")
 
     plt.show()
+
+
+def get_sorted_k_distances(distance_matrix, k):
+    distances = np.empty(len(distance_matrix), dtype=float)
+    for i in range(len(distance_matrix)):
+        d = distance_matrix[i, :]
+        sorted = np.sort(d)
+        # distances[i] = sum(sorted[0:k-1])/k
+        distances[i] = sum(sorted[1:k + 1]) / k
+        # distances[i] = sorted[k+1]
+    distances_sorted = np.sort(distances)
+    return distances_sorted
