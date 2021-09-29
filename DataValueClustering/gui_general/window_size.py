26d55ebffeb917726493b8b13e29d8c58c52edef
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-09-29 15:05:08 +0200

    Correct calculation of window height and max window height

--- DataValueClustering/gui_general/window_size.py
+++ DataValueClustering/gui_general/window_size.py
@@ -1,7 +1,7 @@
 from win32api import GetMonitorInfo, MonitorFromPoint
 
 
-def set_window_size(root, h_expanded=None):
+def set_window_size(root, h_expanded):
     root.update_idletasks()
     width = root.winfo_reqwidth()
     frame_width = root.winfo_rootx() - root.winfo_x()
@@ -16,14 +16,11 @@ def set_window_size(root, h_expanded=None):
     work_area = monitor_info.get("Work")  # work area does not include task bar
     work_area_height = work_area[3]
     title_bar_height = root.winfo_rooty() - root.winfo_y()
-    max_height_inner = work_area_height - title_bar_height + frame_width
+    max_height_inner = work_area_height - title_bar_height #+ frame_width
     h_expanded = h_expanded + frame_width
-    if h_expanded is None:
-        height_inner = max_height_inner
-    else:
-        height_inner = min(max_height_inner, h_expanded)
-    height_outer = height_inner + title_bar_height - frame_width
+    height_inner = min(max_height_inner, h_expanded)
+    height_outer = height_inner + title_bar_height #- frame_width
     y = work_area_height // 2 - height_outer // 2
-    root.maxsize(width, max_height_inner)
+    root.maxsize(width, max_height_inner + frame_width)
     root.geometry('{}x{}+{}+{}'.format(width, height_inner, x, y))
     root.deiconify()
