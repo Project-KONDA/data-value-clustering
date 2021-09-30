e61cf44d23efe1f6fb023d41d931297f8f04f2fe
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-09-30 09:49:34 +0200

    Add set_window_size_simple where height is determined automatically

--- DataValueClustering/gui_general/window_size.py
+++ DataValueClustering/gui_general/window_size.py
@@ -24,3 +24,28 @@ def set_window_size(root, h_expanded):
     root.maxsize(width, max_height_inner + frame_width)
     root.geometry('{}x{}+{}+{}'.format(width, height_inner, x, y))
     root.deiconify()
+
+
+def set_window_size_simple(root):
+    root.update_idletasks()
+    width = root.winfo_reqwidth()
+    frame_width = root.winfo_rootx() - root.winfo_x()
+    window_width = width + 2 * frame_width
+    x = root.winfo_screenwidth() // 2 - window_width // 2
+
+    monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
+    work_area = monitor_info.get("Work")  # work area does not include task bar
+
+    work_area_height = work_area[3]
+    title_bar_height = root.winfo_rooty() - root.winfo_y()
+    max_height_inner = work_area_height - title_bar_height
+
+    y = 0
+    if root.winfo_reqheight() >= max_height_inner:
+        root.geometry('{}x{}+{}+{}'.format(width, max_height_inner, x, y))
+    else:
+        root.geometry('+{}+{}'.format(x, y))
+
+    root.maxsize(width, max_height_inner + frame_width)
+
+    root.deiconify()
