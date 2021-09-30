11b314f1161c16b3fe3afe27b623f88eaa59dc51
Viola Wenz <wenzvi@mathematik.uni-marburg.de>
2021-09-30 10:46:33 +0200

    Rework max window width and position

--- DataValueClustering/gui_general/window_size.py
+++ DataValueClustering/gui_general/window_size.py
@@ -28,24 +28,39 @@ def set_window_size(root, h_expanded):
 
 def set_window_size_simple(root):
     root.update_idletasks()
+
     width = root.winfo_reqwidth()
     frame_width = root.winfo_rootx() - root.winfo_x()
     window_width = width + 2 * frame_width
     x = root.winfo_screenwidth() // 2 - window_width // 2
 
+    height = root.winfo_reqheight()
+
     monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
     work_area = monitor_info.get("Work")  # work area does not include task bar
+
+    work_area_width = work_area[2]
 
     work_area_height = work_area[3]
     title_bar_height = root.winfo_rooty() - root.winfo_y()
     max_height_inner = work_area_height - title_bar_height
 
-    y = 0
-    if root.winfo_reqheight() >= max_height_inner:
+    y = work_area_height // 2 - (root.winfo_reqheight() + title_bar_height) // 2
+    # y = 0
+    # x = - frame_width
+    if height >= max_height_inner and window_width >= work_area_width:
+        x = 0
+        y = 0
+        root.geometry('{}x{}+{}+{}'.format(work_area_width, max_height_inner, x, y))
+    elif height >= max_height_inner:
+        y = 0
         root.geometry('{}x{}+{}+{}'.format(width, max_height_inner, x, y))
+    elif window_width >= work_area_width:
+        x = 0
+        root.geometry('{}x{}+{}+{}'.format(work_area_width, height, x, y))
     else:
         root.geometry('+{}+{}'.format(x, y))
 
-    root.maxsize(width, max_height_inner + frame_width)
+    root.maxsize(work_area_width, max_height_inner + frame_width)
 
     root.deiconify()
